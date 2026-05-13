7341d47925eaf094757ffab676091a844a7d7ae9
Латфулин Денис <d9697335@gmail.com>
2026-05-13 21:19:30 +0300

    FixSheep

--- Assets/Scripts/Gameplay/Workers/States/WorkerGoToResourceState.cs
+++ Assets/Scripts/Gameplay/Workers/States/WorkerGoToResourceState.cs
@@ -8,6 +8,7 @@ public sealed class WorkerGoToResourceState : IWorkerState
     private readonly Worker _worker;
 
     private float _reachDistanceSqr;
+    private float _maxWorkDistanceSqr;
 
     public WorkerGoToResourceState(Worker worker)
     {
@@ -18,39 +19,73 @@ public sealed class WorkerGoToResourceState : IWorkerState
     {
         if (!_worker.HasValidResourceAssignmentForMove())
         {
-            _worker.ClearCurrentAssignment();
-            _worker.StateMachine.ChangeState(WorkerStateType.Idle);
+            ResetToIdle();
             return;
         }
 
         float reachDistance = _worker.GetReachResourceDistance();
-        _reachDistanceSqr = reachDistance * reachDistance;
+        float maxWorkDistance = _worker.GetMaxWorkDistance();
 
-        _worker.Movement.MoveTo(_worker.TargetSlot.Position);
+        _reachDistanceSqr = reachDistance * reachDistance;
+        _maxWorkDistanceSqr = maxWorkDistance * maxWorkDistance;
+
+        bool movementStarted = _worker.Movement.MoveTo(_worker.TargetSlot.Position);
+
+        if (!movementStarted)
+        {
+            ResetToIdle();
+        }
     }
 
     public void Update()
     {
         if (!_worker.HasValidResourceAssignmentForMove())
         {
-            _worker.ClearCurrentAssignment();
-            _worker.StateMachine.ChangeState(WorkerStateType.Idle);
+            ResetToIdle();
             return;
         }
 
-        Vector2 workerPosition = _worker.transform.position;
-        Vector2 targetPosition = _worker.TargetSlot.Position;
+        if (IsCloseEnoughToStartWork(_reachDistanceSqr))
+        {
+            StartWork();
+            return;
+        }
 
-        float sqrDistance = (workerPosition - targetPosition).sqrMagnitude;
-
-        if (sqrDistance > _reachDistanceSqr)
+        if (_worker.Movement.HasTarget)
             return;
 
-        _worker.Movement.Stop();
-        _worker.StateMachine.ChangeState(WorkerStateType.Work);
+        if (IsCloseEnoughToStartWork(_maxWorkDistanceSqr))
+        {
+            StartWork();
+            return;
+        }
+
+        ResetToIdle();
     }
 
     public void Exit()
     {
+    }
+
+    private bool IsCloseEnoughToStartWork(float distanceSqr)
+    {
+        Vector2 workerPosition = _worker.transform.position;
+        Vector2 targetPosition = _worker.TargetSlot.Position;
+
+        float currentDistanceSqr = (workerPosition - targetPosition).sqrMagnitude;
+
+        return currentDistanceSqr <= distanceSqr;
+    }
+
+    private void StartWork()
+    {
+        _worker.Movement.Stop();
+        _worker.StateMachine.ChangeState(WorkerStateType.Work);
+    }
+
+    private void ResetToIdle()
+    {
+        _worker.ClearCurrentAssignment();
+        _worker.StateMachine.ChangeState(WorkerStateType.Idle);
     }
 }
